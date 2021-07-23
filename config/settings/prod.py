c28aaf6c50161f5421005a469e88443f1d6d0a8a
hansk111 <hanseokhee.han@gmail.com>
2021-07-23 20:05:34 +0900

    settings change

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 
-ALLOWED_HOSTS = ['13.124.33.29', 'smarthan.site']
+ALLOWED_HOSTS = ['52.78.44.13', 'smarthan.site']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
