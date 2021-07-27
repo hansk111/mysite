40aece3d5bba9556d92b0a7f88e0fede47afa606
hansk111 <hanseokhee.han@gmail.com>
2021-07-27 21:48:27 +0900

    mariadb

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -4,3 +4,23 @@ ALLOWED_HOSTS = ['3.38.71.222', 'smarthan.site']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
+
+DATABASES = {
+      'default': {
+          'ENGINE': 'django.db.backends.mysql',
+          'NAME': 'smarthandb',
+          'HOST': 'localhost',
+          'PORT': '3306',
+          'USER': 'root',
+          'PASSWORD': 'admin'
+      }
+  }
+
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
