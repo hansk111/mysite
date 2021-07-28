00aac86764023bea47b2d5e1a0b643b8cb57c6db
hansk111 <hanseokhee.han@gmail.com>
2021-07-28 19:21:24 +0900

    mariadb change

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -25,13 +25,27 @@ DEBUG = False
 #     }
 # }
 
-DATABASES = {
+# DATABASES = {
+#       'default': {
+#           'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#           'NAME': 'mysite',
+#           'HOST': 'localhost',
+#           'PORT': '5432',
+#           'USER': 'han',
+#           'PASSWORD': 'han'
+#       }
+# }
+
+
+
+# root password : admin
+  DATABASES = {
       'default': {
-          'ENGINE': 'django.db.backends.postgresql_psycopg2',
-          'NAME': 'mysite',
+          'ENGINE': 'django.db.backends.mysql',
+          'NAME': 'smarthandb',
           'HOST': 'localhost',
-          'PORT': '5432',
-          'USER': 'han',
-          'PASSWORD': 'han'
+          'PORT': '3306',
+          'USER': 'root',
+          'PASSWORD': 'admin'
       }
-}
+  }
