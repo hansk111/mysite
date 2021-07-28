ac27cf65729d4c05b447ee96410a1b96397b5595
hansk111 <hanseokhee.han@gmail.com>
2021-07-28 20:15:52 +0900

    post

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -25,27 +25,27 @@ DEBUG = False
 #     }
 # }
 
-# DATABASES = {
-#       'default': {
-#           'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#           'NAME': 'mysite',
-#           'HOST': 'localhost',
-#           'PORT': '5432',
-#           'USER': 'han',
-#           'PASSWORD': 'han'
-#       }
-# }
+DATABASES = {
+      'default': {
+          'ENGINE': 'django.db.backends.postgresql_psycopg2',
+          'NAME': 'mysite',
+          'HOST': 'localhost',
+          'PORT': '5432',
+          'USER': 'han',
+          'PASSWORD': 'han'
+      }
+}
 
 
 
 # root password : admin
-DATABASES = {
-      'default': {
-          'ENGINE': 'django.db.backends.mysql',
-          'NAME': 'smarthandb',
-          'HOST': 'localhost',
-          'PORT': '3306',
-          'USER': 'root',
-          'PASSWORD': 'admin'
-      }
-}
+# DATABASES = {
+#       'default': {
+#           'ENGINE': 'django.db.backends.mysql',
+#           'NAME': 'smarthandb',
+#           'HOST': 'localhost',
+#           'PORT': '3306',
+#           'USER': 'root',
+#           'PASSWORD': 'admin'
+#       }
+# }
