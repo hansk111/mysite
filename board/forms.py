9b95b602f94852320b7822756118ef0a4bfd96b8
한석희(SEOKHEE HAN) <hansk111.han@hanwha.com>
2021-08-03 12:59:14 +0900

    ckeditor change

--- board/forms.py
+++ board/forms.py
@@ -7,17 +7,19 @@ from board.models import Post, Comment
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['subject','head_image', 'content', ]
+        fields = ['subject','head_image', 'content','category' ]
 
-        # widgets = {
-        #     'subject': forms.TextInput(attrs={'class': 'form-control'}),
-        #     'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 10}),
-        # }
+        widgets = {
+            'subject': forms.TextInput(attrs={'class': 'form-control'}),
+            'head_image': forms.FileInput(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 10}),
+        }
 
         labels = {
             'subject': '제목',
-            'head_image': '이미지',
+            'head_image': '대표 이미지 선택',
             'content': '내용',
+            'category': '카테고리',
             
          
         }
