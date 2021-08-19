787029a95e701839ccaeda09d0cf3e56b45634ad
hansk111 <hanseokhee.han@gmail.com>
2021-08-19 21:11:25 +0900

    debug

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -3,7 +3,7 @@ from .base import *
 ALLOWED_HOSTS = ['3.38.71.222', 'smarthan.site']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
-DEBUG = False
+DEBUG = True
 
 # DATABASES = {
 #       'default': {
